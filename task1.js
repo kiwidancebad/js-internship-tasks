9e9043ba1ed4aa513bfadcd9a6d698d65f417f4c
kiwidancebad <morelessequal@gmail.com>
2020-08-04 20:45:10 +0300

    feat: task 1

---
+++ task1.js
@@ -0,0 +1,12 @@
+/**
+ * Дана строка, написать функцию которая за один проход по строке вернет первый
+ * символ встретившийся второй раз
+ */
+
+function firstRecurringChar(str) {
+  /** Код... */
+}
+
+console.log(firstRecurringChar("abcdefa") === "a");
+console.log(firstRecurringChar("gHRirHS") === "H");
+console.log(firstRecurringChar("e g jrd g") === "g");
